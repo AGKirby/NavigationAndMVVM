b238d8f86b454050b747f1fbeebc6eec6a67918c
Jonathan Mach <jonathan.mach00@gmail.com>
2022-11-11 09:48:23 -0800

    Add Profile page blank template

--- app/src/main/java/com/example/navigationandmvvm/ViewsAndViewModels/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/navigationandmvvm/ViewsAndViewModels/notifications/NotificationsFragment.kt
@@ -1,20 +1,23 @@
 package com.example.navigationandmvvm.ViewsAndViewModels.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.navigationandmvvm.ViewsAndViewModels.profiles.ProfilesFragment
+import com.example.navigationandmvvm.ViewsAndViewModels.profiles.ProfilesViewModel
 import com.example.navigationandmvvm.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -32,6 +35,15 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // TODO: Delete afterwards
+        val navProfileBtn: Button = binding.navProfileBtn
+        navProfileBtn.setOnClickListener {
+            // Navigate to Profile Page
+            val intent = Intent(activity, ProfilesFragment::class.java)
+            startActivity(intent)
+        }
+
         return root
     }
 
